1ad8fc5975fa3f7c96d64b3851874d7f2b92c4b6
khabiirk <63027274+khabiirk@users.noreply.github.com>
2020-06-25 22:28:58 -0400

    Add files via upload

--- Tic-Tac-Toe/script.js
+++ Tic-Tac-Toe/script.js
@@ -1,8 +1,19 @@
 const gameBoard = (() => {
-  var board = [];
+  var board = new Array(9);
   let turn1;
   let turn2;
   let turn;
+  let rec = 0;
+  const COMBOS = [
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
   function getBoard(){
     return board;
   }
@@ -24,13 +35,135 @@ const gameBoard = (() => {
     }
 
   }
+  function emptySpaces(){
+    let e = []
+    for(let c = 0; c < 9; c++){
+      //alert(!board[c]);
+      if(!board[c]){
+        e.push(c)
+      }
+    }
+    //alert(e);
+    return e;
+  }
+  function iswinner(p){
+    for(let i = 0; i < COMBOS.length; i++){
+      let won = true;
+      for(let j = 0; j < COMBOS[i].length; j++){
+        let id = COMBOS[i][j];
+        won = board[id] == p && won;
+      }
+      if (won){
+        turn1 = "none";
+        //alert("iswinner" + true)
+        return true;
+      }
+    }
+    //alert("iswinner " + false)
+    return false;
+  }
+
+  function isTie(p){
+    let isBoardFill = true;
+    for(let i = 0; i < board.length; i++){
+      isBoardFill = board[i] && isBoardFill;
+    }
+    if(isBoardFill){
+      //alert("istie" + true)
+      turn1 = "none";
+      return true;
+    }
+    //alert("istie" + false)
+    return false;
+  }
+
+  function minimax(p){
+    //alert('going here');
+
+    if(iswinner('O')){
+      //alert('winner');
+      return {evaluation: -10};
+    }
+    else if(iswinner('X')){
+      return {evaluation: +10};
+    }
+    else if(isTie('Player 1')){
+      return {evaluation: 0};
+    }
+    let EMPTY = emptySpaces();
+    //console.table(EMPTY);
+    let moves = []
+
+
+    for (var i = 0; i < EMPTY.length; i++){
+      let id = EMPTY[i];
+      let move = {};
+
+      move.id = id;
+      let savedBoardSpace = board[id];
+      board[id] = p;
+      //render();
+
+      if(p == "X"){
+
+        //alert('here');
+
+        move.evaluation = minimax('O').evaluation;
+      }
+      else{
+
+        move.evaluation = minimax('X').evaluation;
+      }
+      //(move.evaluation);
+
+      board[id] = savedBoardSpace;
+      moves.push(move);
+      //console.log("Evaluation is " + move.evaluation);
+    }
+  //  console.log("out " + i);
+  //  console.table(moves);
+    let bestMove;
+    if(p == "X"){
+      //alert('Here X')
+      let max = -9999999;
+      for(let count = 0; count < moves.length; count++){
+        if(moves[count].evaluation > max ){
+          //alert("in");
+          max = moves[count].evaluation;
+          bestMove = moves[count];
+        }
+      }
+      //alert(bestMove);
+    }
+    else{
+      //alert('Here O')
+      let min = 999999999;
+      for(let count = 0; count < moves.length; count++){
+        if(moves[count].evaluation < min){
+
+          min = moves[count].evaluation;
+          bestMove = moves[count];
+        }
+      }
+      //alert(bestMove);
+    }
+  //  console.log(bestMove.evaluation);
+    rec++
+  //  console.log(rec + "st");
+
+  //  console.table(bestMove);
+    return bestMove;
+
+
+  }
   const player = (move) =>{
 
-    function play(cell){
+    function play(c){
     //  alert(typeof(board[parseInt(cell.id)]));
-      if (typeof(board[parseInt(cell.id)]) == 'undefined'){
+      if (typeof(board[parseInt(c)]) == 'undefined'){
     //    alert('in');
-        board[parseInt(cell.id)] = move;
+        board[parseInt(c)] = move;
+
 
 
         render();
@@ -42,37 +175,129 @@ const gameBoard = (() => {
   function game(){
     const player1 = player('O');
     const player2 = player('X');
+    let a = document.createElement('Button');
+    let m = document.querySelector('section');
+    a.classList.add('a');
+    var aiCheck = false;
+    a.textContent = "AI";
+    m.appendChild(a);
+    a.addEventListener('click', ()=>{
+      aiCheck = true;
+
+
+    });
+
     let cell = document.querySelectorAll('.play-cell');
+
+
+    turn1 = "Player 1"
 
     for(let j = 0; j < 9; j++){
       let v = document.querySelector('.head');
-      if(turn1){
-        v.textContent = "Player 1 turn";
+      if(turn1 != "none"){
+        v.textContent = turn1 + " turn";
       }
-      else if(turn2){
-        v.textContent = "Player 2 turn";
-      }
+
       cell[j].addEventListener('click', ()=>{
 
-        if(turn1){
-          player1.play(cell[j]);
+        a.classList.add("remove")
+        if(turn1 == "Player 1"){
+          //alert("dd");
+          player1.play(cell[j].id);
           cell[j].setAttribute('value', 0);
-          turn1 = false;
-          turn2 = true;
-          win_condition();
-          turn++;
-          v.textContent ='Player 2 turn';
+          let l = iswinner('O');
+          turn1 = "Player 2";
+          v.textContent = turn1 + " turn";
+          if(l){
+            alert('Player 1 win');
+            turn1 = "none";
+
+          }
+
+          let ai;
+          if(aiCheck){
+            ai = minimax('X').id;
+            console.log(ai);
+            player2.play(ai);
+            turn1 = "Player 1";
+            let y = iswinner('X');
+            if(y){
+              alert('You lose');
+              turn1 = "none";
+
+            }
+            if(isTie()){
+              alert('Draw');
+              turn1 = "none";
+            }
+
+
+          }
+
+
 
         }
-        else if(turn2){
-          player2.play(cell[j]);
-          cell[j].setAttribute('value', 1);
-          turn2 = false;
-          turn1 = true;
-          win_condition();
-          turn++;
-          v.textContent ='Player 1 turn';
+        else if (turn1 == "Player 2") {
+          player2.play(cell[j].id);
+          turn1 = "Player 1";
+          let y = iswinner('X');
+          v.textContent = turn1 + " turn";
+          if(y){
+            alert('You lose');
+            turn1 = "none";
+
+          }
+          if(isTie()){
+            alert('Draw');
+            turn1 = "none";
+          }
         }
+
+        if(turn1 == "none"){
+          let d = document.querySelector('section');
+          let bu = document.createElement('button');
+          bu.textContent = 'Reset';
+          turn1 = 'h';
+
+          d.appendChild(bu);
+          //
+          //
+
+
+          bu.addEventListener('click', ()=>{
+        //    for(let i = 0; i < board.length; i++){
+        //      board.splice(i,1);
+        //    }
+            board.length = 0;
+            turn1 = "Player 1";
+
+            console.table(board);
+
+            let m = document.querySelector('.board');
+            d.innerHTML = '';
+            m.innerHTML = '';
+
+            createBoard();
+
+            //d.removeChild(r);
+            render();
+            game();
+          });
+
+        }
+
+  //        v.textContent ='Player 2 turn';
+
+//        }
+//        else if(turn2){
+//          player2.play(cell[j]);
+//          cell[j].setAttribute('value', 1);
+//          turn2 = false;
+//          turn1 = true;
+//          win_condition();
+//          turn++;
+//          v.textContent ='Player 1 turn';
+//        }
 
 
       })
@@ -87,6 +312,9 @@ const gameBoard = (() => {
     for(let i = 0; i < board.length; i++){
       d[i].textContent = '';
       //alert(board[i]);
+      if(board[i] == "X"){
+        d[i].style.color = "red";
+      }
       d[i].textContent = board[i];
     }
 
@@ -94,24 +322,28 @@ const gameBoard = (() => {
   }
   function win_condition(){
 
-
+    let winner;
 
     if((board[0] == board[1]) && (board[1] == board[2]) && (board[0] == board[2]) && typeof(board[0]) != 'undefined'){
 
       if(board[0] == 'O'){
         alert("Player 1 wins");
+        winner = 'Player 1';
       }
       else{
         alert("Player 2 wins");
+        winner = 'Player 2'
       }
     }
     else if((board[3] == board[4]) && (board[4] == board[5]) && (board[3] == board[5])&& typeof(board[3]) != 'undefined'){
 
       if(board[3] == 'O'){
         alert("Player 1 wins");
+        winner = 'Player 1';
       }
       else{
         alert("Player 2 wins");
+        winner = 'Player 2';
 
     }
   }
@@ -119,9 +351,11 @@ const gameBoard = (() => {
 
       if(board[6] == 'O'){
         alert("Player 1 wins");
+        winner = 'Player 1';
       }
       else{
         alert("Player 2 wins");
+        winner = 'Player 2';
 
     }
   }
@@ -129,50 +363,61 @@ const gameBoard = (() => {
 
       if(board[0] == 'O'){
         alert("Player 1 wins");
+        winner = 'Player 1';
       }
       else{
         alert("Player 2 wins");
+        winner = 'Player 2';
 
     }
   }
     else if((board[2] == board[4]) && (board[4] == board[6]) && (board[2] == board[6])&& typeof(board[2]) != 'undefined'){
       if(board[2] == 'O'){
         alert("Player 1 wins");
+        winner = 'Player 1';
       }
       else{
         alert("Player 2 wins");
+        winner = 'Player 2';
 
     }}
 
     else if((board[0] == board[3]) && (board[3] == board[6]) && (board[0] == board[6])&& typeof(board[0]) != 'undefined'){
       if(board[0] == 'O'){
         alert("Player 1 wins");
+        winner = 'Player 1';
       }
       else{
         alert("Player 2 wins");
+        winner = 'Player 2';
 
     }
   }
     else if((board[1] == board[4]) && (board[4] == board[7]) && (board[1] == board[7])&& typeof(board[1]) != 'undefined'){
       if(board[1] == 'O'){
         alert("Player 1 wins");
+        winner = 'Player 1';
       }
       else{
         alert("Player 2 wins");
+        winner = 'Player 2';
 
     }}
     else if((board[2] == board[5]) && (board[5] == board[8]) && (board[2] == board[8])&& typeof(board[2]) != 'undefined'){
       if(board[2] == 'O'){
         alert("Player 1 wins");
+        winner = 'Player 1';
       }
       else{
         alert("Player 2 wins");
+        winner = 'Player 2';
 
     }
 
   }
     else if(turn == 9){
       alert("Draw");
+      winner = "Draw";
     }
     else {
       return false;
@@ -207,11 +452,11 @@ const gameBoard = (() => {
       render();
       game();
     });
-    return true;
+    return winner;
 
 
   }
-  
+
 
   return {
     createBoard,
